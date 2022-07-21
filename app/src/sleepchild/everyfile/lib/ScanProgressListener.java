131ce666a4c564fcee4e08d0445dd19dea7d18a8
Collins Mucho <72305974+sleepchild@users.noreply.github.com>
2022-07-21 19:15:43 +0200

    Delete app directory

--- app/src/sleepchild/everyfile/lib/ScanProgressListener.java
+++
@@ -1,9 +0,0 @@
-package sleepchild.everyfile.lib;
-import java.util.*;
-import java.io.*;
-
-public interface ScanProgressListener{
-    public void onScanStarted();
-    public void onScanResultsUpdated(List<File> list);
-    public void onScanCompleted();
-}
--- app/src/sleepchild/everyfile/lib/SearchResultListener.java
+++
@@ -1,9 +0,0 @@
-package sleepchild.everyfile.lib;
-
-import java.util.*;
-import java.io.*;
-
-public interface SearchResultListener
-{
-    public void onSearchResult(List<File> result);
-}
